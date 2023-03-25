6f06fea4834bb916fe80abe9f4a1beaacefed3bb
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-03-25 13:01:24 +0100

    fix script.js

--- assets-slider/js/script.js
+++ assets-slider/js/script.js
@@ -6,11 +6,15 @@
 5. Click next aggiunge .d-none, aumenta il contatore di 1, rimuove il d-none
 6. Click prev aggiunge .d-none, diminuisce il contatore di 1, rimuove il d-none
 7. Se il contatore è 0, assume il valore della lunghezza della lista e viceversa
-8. Variabile active per le card , inserita nei click
+8. Variabile listaCard per le card , inserita nei click
 9. Variabile opacity per i layer, inserita nei click
 10. listaCard e selezione immagine tramite click su ogni singola card
 11. Ottimizzare selezione immagine tramite click con ciclo for
 */
+
+
+
+// Caricare immagini
 
 const listaImmagini = [
   'assets-slider/img/01.webp',
@@ -23,11 +27,6 @@ const listaImmagini = [
 const full = document.querySelector('.full');
 const slider = document.querySelector('.slider');
 
-let contatore = 0;
-
-
-
-// Caricare immagini
 for (let i = 0; i < listaImmagini.length; i ++) {
 
   let immagine = listaImmagini[i];
@@ -43,30 +42,33 @@ for (let i = 0; i < listaImmagini.length; i ++) {
 
 
 
-// Rendere visibile la prima di default
+let contatore = 0;
+
+
+
+// Rendere visibile, active e togliere l'opacity alla prima di default
+
 const img = document.getElementsByClassName('img');
 img[contatore].classList.remove('d-none');
 
-// Rendere active la prima card di default
-const active = document.getElementsByClassName('card');
-active[contatore].classList.add('active');
-
-// Togliere opacity alla prima card di default
 const opacity = document.getElementsByClassName('layer');
 opacity[contatore].classList.remove('opacity');
 
+const listaCard = document.getElementsByClassName('card');
+listaCard[contatore].classList.add('active');
 
 
-// Frecce prev e next
+
+// Cick prev e next
+
 const prev = document.querySelector('.arrow.prev');
 const next = document.querySelector('.arrow.next');
 
-// Click tasto next
 next.addEventListener('click', function() {
 
   img[contatore].classList.add('d-none');
-  active[contatore].classList.remove('active');
   opacity[contatore].classList.add('opacity');
+  listaCard[contatore].classList.remove('active');
 
   if (contatore === img.length - 1){
     contatore = 0;
@@ -75,17 +77,16 @@ next.addEventListener('click', function() {
   }
 
   img[contatore].classList.remove('d-none');  
-  active[contatore].classList.add('active');
   opacity[contatore].classList.remove('opacity');
+  listaCard[contatore].classList.add('active');
 
 })
 
-// Click tasto prev
 prev.addEventListener('click', function() {
 
   img[contatore].classList.add('d-none');
-  active[contatore].classList.remove('active');
   opacity[contatore].classList.add('opacity');
+  listaCard[contatore].classList.remove('active');
 
   if (contatore === 0){
     contatore = img.length - 1;
@@ -94,15 +95,14 @@ prev.addEventListener('click', function() {
   }
 
   img[contatore].classList.remove('d-none');
-  active[contatore].classList.add('active');
   opacity[contatore].classList.remove('opacity');
+  listaCard[contatore].classList.add('active');
   
 })
 
 
 
 // Selezionare immagine tramite click sulla stessa
-const listaCard = document.getElementsByClassName('card');
 
 for (let n = 0; n < listaCard.length; n++) {
 
@@ -110,14 +110,14 @@ for (let n = 0; n < listaCard.length; n++) {
   
   if (contatore != listaCard[n]){
     img[contatore].classList.add('d-none');
-    active[contatore].classList.remove('active');
     opacity[contatore].classList.add('opacity');  
+    listaCard[contatore].classList.remove('active');
     
     contatore = n;
     
     img[contatore].classList.remove('d-none');  
-    active[contatore].classList.add('active');
     opacity[contatore].classList.remove('opacity');
+    listaCard[contatore].classList.add('active');
   }
       
 })
